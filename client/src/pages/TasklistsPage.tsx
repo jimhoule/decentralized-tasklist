3dd33a88d3833b1613fca9bae5eb0c7e3c8350f0
Jimmy Houle <jimhoule09@gmail.com>
2024-07-06 17:23:57 -0400

    tasklists page skeleton

--- client/src/pages/TasklistsPage.tsx
+++ client/src/pages/TasklistsPage.tsx
@@ -1,5 +1,58 @@
 export function TasklistsPage(): JSX.Element {
     return (
-        <h1>Tasklists!!!</h1>
+        <div
+            className={`
+                container
+                bg-gray-700
+                mt-20
+                p-8
+                rounded-md
+            `}
+        >
+            <h1
+                className={`
+                    text-white
+                    mb-5
+                `}
+            >
+                list name
+            </h1>
+
+            <h1
+                className={`
+                    text-white
+                    mb-5
+                `}
+            >
+                list description
+            </h1>
+
+            <h1
+                className={`
+                    text-white
+                    mb-5
+                `}
+            >
+                email
+            </h1>
+
+            <div
+                className={`
+                    flex
+                    justify-between
+                    items-center
+                    bg-violet-800
+                    text-white
+                    py-3
+                    px-4
+                    rounded-md
+                    mb-1
+                    cursor-pointer
+                `}
+            >
+                <p>task name</p>
+                <p>is done</p>
+            </div>
+        </div>
     );
 }

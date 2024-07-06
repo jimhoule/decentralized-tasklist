54572ef3f6dcbba2e40148f037d3685941af53e0
Jimmy Houle <jimhoule09@gmail.com>
2024-07-06 17:25:19 -0400

    adding global layout to router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,41 @@
-import { Routes } from './routes';
+import { Outlet } from 'react-router-dom';
+import { NavBar } from './components/NavBar';
 
 export function App(): JSX.Element {
     return (
         <div
             className={`
                 container
+                w-screen
+                h-screen
+                max-w-full
+                flex
+                flex-col
             `}
         >
-            <Routes />
+            <div
+                className={`
+                    container
+                    w-full
+                    h-auto
+                    max-w-full
+                    bg-black
+                `}
+            >
+                <NavBar />
+            </div>
+
+            <div
+                className={`
+                    container
+                    w-full
+                    h-full
+                    max-w-full
+                    bg-black
+                `}
+            >
+                <Outlet/>
+            </div>
         </div>
     );
 }
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,24 +1,31 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from './App';
 import { HomePage } from './pages/HomePage';
 import { TasklistsPage } from './pages/TasklistsPage';
 import { WalletsPage } from './pages/WalletsPage';
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-        path: "/tasklists",
-        element: <TasklistsPage />,
-    },
-    {
-        path: "/wallets",
-        element: <WalletsPage />,
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: '/tasklists',
+                element: <TasklistsPage />,
+            },
+            {
+                path: '/wallets',
+                element: <WalletsPage />,
+            },
+        ],
     },
 ]);
 
-export function Routes(): JSX.Element {
+export function Router(): JSX.Element {
     return (
         <RouterProvider router={router}/>
     );

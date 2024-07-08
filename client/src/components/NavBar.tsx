3d77884a98633ea3223aafed3fb014da55f6098d
Jimmy Houle <jimhoule09@gmail.com>
2024-07-08 16:20:34 -0400

    NavBAr compoent adjusments

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,12 +2,38 @@ import { NavLink } from './NavLink';
 
 export function NavBar(): JSX.Element {
     return (
-        <nav className="bg-gray-900 w-full h-full p-2 flex">
-            <div className="flex items-center mr-3">
-                <img className="h-8 w-auto" src="./assets/eth.png" alt="eth" />
+        <nav
+            className={`
+                bg-gray-900
+                w-full
+                h-full
+                p-2
+                flex
+            `}
+        >
+            <div
+                className={`
+                    flex
+                    items-center
+                    mr-3
+                `}
+            >
+                <img 
+                    className={`
+                        h-8 
+                        w-auto
+                    `}
+                    src='../assets/eth.png'
+                    alt='eth'
+                />
             </div>
 
-            <div className="flex space-x-4">
+            <div
+                className={`
+                    flex
+                    space-x-4
+                `}
+            >
                 <NavLink path='/' text='Home' />
                 <NavLink path='/tasklists' text='Tasklists' />
                 <NavLink path='/wallets' text='Wallets' />

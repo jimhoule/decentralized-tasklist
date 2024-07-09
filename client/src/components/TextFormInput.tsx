5063afb8846c8b31778c80712530e4a3447b0a84
Jimmy Houle <jimhoule09@gmail.com>
2024-07-09 10:09:24 -0400

    adding placeholder props to TextFormInput component

--- client/src/components/TextFormInput.tsx
+++ client/src/components/TextFormInput.tsx
@@ -16,6 +16,21 @@ export function TextFormInput(
     return (
         <input
             type='text'
+            className={`
+                mb-2
+                px-1
+                py-2
+                border-2
+                border-gray-900
+                rounded-md
+                w-3/4
+                focus:placeholder-white
+                focus:bg-gray-400
+                focus:text-white
+                focus:py-3
+                ease-in-out
+                duration-200
+            `}
             placeholder={placeholder}
             value={value}
             onChange={onChange}
